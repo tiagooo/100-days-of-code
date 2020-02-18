df56dc8c6d64c8750e7bd23efc1598030dcf2a3f
tiagooo <tmagalhaes@lugardajoia.com>
2020-02-18 17:04:44 +0000

    byte 251 initial soling

--- exercises/week4/byte_251.py
+++ exercises/week4/byte_251.py
@@ -1,1 +1,45 @@
 # bite 251. Introducing Pandas Series
+
+import string
+
+import pandas as pd
+
+
+def basic_series():
+    """Create a pandas Series containing the values 1, 2, 3, 4, 5
+    Don't worry about the indexes for now.
+    The name of the series should be 'Fred'
+    """
+    basicseries = pd.Series([1, 2, 3, 4, 5], dtype='int64', name='Fred')
+    return basicseries
+
+
+def float_series():
+    """Create a pandas Series containing the all the values
+    from 0.000 -> 1.000 e.g. 0.000, 0.001, 0.002... 0.999, 1.000
+    Don't worry about the indexes or the series name.
+    """
+    rangearray = [*range(0, 1001)]
+    decimalarray = [i/1000 for i in rangearray]
+    floatseries = pd.Series(decimalarray)
+    return floatseries
+
+
+def alpha_index_series():
+    """Create a Series with values 1, 2, ... 25, 26 of type int64
+    and add an index with values a, b, ... y, z
+    so index 'a'=1, 'b'=2 ... 'y'=25, 'z'=26
+    Don't worry about the series name.
+    """
+    alfaseries = pd.Series(range(1, 27), ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+    return alfaseries
+
+
+def object_values_series():
+    """Create a Series with values A, B, ... Y, Z of type object
+    and add an index with values 101, 102, ... 125, 126
+    so index 101='A', 102='B' ... 125='Y', 126='Z'
+    Don't worry about the series name.
+    """
+    objectseries = pd.Series(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'], range(101, 127))
+    return objectseries
